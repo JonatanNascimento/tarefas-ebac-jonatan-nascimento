4522e906bac958d053b66463be7d19bff21a1d21
Jonatan Nascimento <quadrado4@gmail.com>
2023-04-06 23:07:19 -0300

    Primeira tarefa

---
+++ src/Jonatan/Nas/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package Jonatan.Nas;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.print("Hello Jonatan");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author quadr
+ *
+ */
+module PrimeiroProjeto {
+}
